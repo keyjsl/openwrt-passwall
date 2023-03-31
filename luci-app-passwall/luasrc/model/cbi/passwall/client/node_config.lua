e06f9fac5e8cec9e493f34ea33316f5ebe657386
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-03-31 09:42:38 +0800

    Update node_config.lua

--- luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
@@ -528,27 +528,17 @@ xtls:depends({ type = "Xray", protocol = "trojan", tls = true })
 
 flow = s:option(Value, "flow", translate("flow"))
 flow.default = "xtls-rprx-direct"
-flow:value("xtls-rprx-vision")
-flow:value("xtls-rprx-vision-udp443")
+if queryParam.flow == "xtls-rprx-vision"
+    flow.default = "xtls-rprx-vision"
 flow:value("xtls-rprx-origin")
 flow:value("xtls-rprx-origin-udp443")
 flow:value("xtls-rprx-direct")
 flow:value("xtls-rprx-direct-udp443")
 flow:value("xtls-rprx-splice")
 flow:value("xtls-rprx-splice-udp443")
+flow:value("xtls-rprx-vision")
+flow:value("xtls-rprx-vision-udp443")
 flow:depends("xtls", true)
-
-#vtls = s:option(Flag, "vtls", translate("vTLS"))#
-#vtls.default = 0#
-#vtls:depends({ type = "Xray", protocol = "vless", tls = true })#
-#vtls:depends({ type = "Xray", protocol = "trojan", tls = true })#
-
-#tlsflow = s:option(Value, "tlsflow", translate("tlsflow"))#
-#tlsflow.default = "xtls-rprx-vision"#
-#tlsflow:value("none")#
-#tlsflow:value("xtls-rprx-vision")#
-#tlsflow:value("xtls-rprx-vision-udp443")#
-#tlsflow:depends("vtls", true)#
 
 alpn = s:option(ListValue, "alpn", translate("alpn"))
 alpn.default = "default"
